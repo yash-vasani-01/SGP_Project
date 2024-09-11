8eeb16b0b751dcb5cb1ba2b0b62d600abaa5db6c
yash-vasani-01 <vasaniyash784@gmail.com>
2024-09-11 22:58:43 +0530

    update 1

--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
-
 from django.urls import path
-from .views import login_,logout_,register,home,record,role,welcome,admin_page,faculty_page
+from .views import login_,logout_,register,home,record,role,welcome,admin_page,faculty_page,add_seminar_hall,get_hall_details_by_name,institute_info,book_hall
 
 urlpatterns = [
     path('',welcome,name='welcome'),
@@ -12,4 +11,9 @@ urlpatterns = [
     path('role/',role,name='role'),
     path('admin_/',admin_page,name='admin_page'),
     path('faculty/',faculty_page,name='faculty_page'),
+    path('add_hall/',add_seminar_hall,name='add_hall'),
+    path('get_hall_details_by_name/<str:hall_name>/<str:institute_name>/', get_hall_details_by_name, name='get_hall_details_by_name'),
+    path('institute_info/<str:institute_name>/', institute_info, name='institute_info'),
+    path('book_hall/<str:hall_name>/<str:institutename>/',book_hall,name='book_hall'), 
+    
 ]
